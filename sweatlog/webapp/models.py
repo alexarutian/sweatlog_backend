23cdb0e621d4e9616e4b0356a530d529cb0bb575
Alex Arutian <alexarutian@gmail.com>
2022-02-20 23:51:05 -0800

    basic fixture loading

--- sweatlog/webapp/models.py
+++ sweatlog/webapp/models.py
@@ -1,22 +1,39 @@
 from django.db import models
 
+# A natural key is a tuple of values that can be used to uniquely identify an object instance without using the primary key value.
+# # use for handling of fixtures by Django - will check to see if there is a get_by_natural_key method
+
+
+class ExerciseTypeManager(models.Manager):
+    def get_by_natural_key(self, name):
+        return self.get(name=name)
+
 
 class ExerciseType(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
+
+    objects = ExerciseTypeManager()
 
     def __str__(self):
         return self.name
 
 
+class EquipmentTypeManager(models.Manager):
+    def get_by_natural_key(self, name):
+        return self.get(name=name)
+
+
 class EquipmentType(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
+
+    objects = EquipmentTypeManager()
 
     def __str__(self):
         return self.name
 
 
 class Exercise(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     description = models.CharField(max_length=200, blank=True)
     exercise_type = models.ForeignKey(
         ExerciseType,
@@ -50,10 +67,10 @@ class BlockContent(models.Model):
     block = models.ForeignKey(
         Block, on_delete=models.CASCADE, related_name="blockcontents"
     )
-    suggested_sets = models.IntegerField(null=True)
-    suggested_reps = models.IntegerField(null=True)
-    suggested_weight_lb = models.FloatField(null=True)
-    suggested_time_in_seconds = models.IntegerField(null=True)
+    suggested_sets = models.IntegerField(null=True, blank=True)
+    suggested_reps = models.IntegerField(null=True, blank=True)
+    suggested_weight_lb = models.FloatField(null=True, blank=True)
+    suggested_time_in_seconds = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.exercise.name + "-" + self.block.name
@@ -90,12 +107,13 @@ class WorkoutLog(models.Model):
     exercise = models.ForeignKey(
         Exercise, on_delete=models.CASCADE, related_name="workoutlogs"
     )
-    actual_sets = models.IntegerField(null=True)
-    actual_reps = models.IntegerField(null=True)
-    actual_weight_lb = models.FloatField(null=True)
-    actual_time_in_seconds = models.IntegerField(null=True)
+    actual_sets = models.IntegerField(null=True, blank=True)
+    actual_reps = models.IntegerField(null=True, blank=True)
+    actual_weight_lb = models.FloatField(null=True, blank=True)
+    actual_time_in_seconds = models.IntegerField(null=True, blank=True)
     notes = models.TextField(blank=True)
     date = models.DateTimeField(auto_now=True)
 
+    # add date logic to this later
     def __str__(self):
-        return self.workout.name + "-" + self.date
+        return self.workout.name
